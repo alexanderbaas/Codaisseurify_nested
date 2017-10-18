302f67ab9acde01290fc069a4e45543d84e0ac43
Alexander Baas <ajmbaas@outlook.com>
2017-10-18 18:34:32 +0200

    Create Song Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018163155) do
+ActiveRecord::Schema.define(version: 20171018163414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20171018163155) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "remote_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
