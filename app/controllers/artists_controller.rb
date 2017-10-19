3560fef02d780e875bbf8bb494c2dbc8110c7029
Alexander Baas <ajmbaas@outlook.com>
2017-10-19 09:24:51 +0200

    Create Index Page Artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,19 @@
 class ArtistsController < ApplicationController
+before_action :find_artist, only: [:show, :destroy]
+
+    def index
+     @artists = Artist.all
+    end
+
+    def show
+    end
+
+
+
+    private
+
+    def find_artist
+    @artist = Artist.find(params[:id])
+    end
+
 end
