089f82be60e25a3ac3cac1ff5ce5c59f8fd362bb
Alexander Baas <ajmbaas@outlook.com>
2017-10-19 09:36:21 +0200

    Create Show Controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,34 @@
 class SongsController < ApplicationController
+before_action :find_artist, only: [:create]
+
+  def new
+     @song = Song.new
+   end
+
+  def create
+      @song = @artist.songs.new(song_params)
+
+    if @song.save
+        redirect_to @artist
+      else
+        render :new
+      end
+    end
+
+   def destroy
+      @song = Song.find(params[:id])
+      @song.destroy
+      redirect_to artists_path
+    end
+
+  private
+
+  def find_artist
+     @artist = Artist.find(params[:artist_id])
+   end
+
+  def song_params
+     song_params = params.require(:song).permit(:title, :artist_id)
+   end
+
 end
